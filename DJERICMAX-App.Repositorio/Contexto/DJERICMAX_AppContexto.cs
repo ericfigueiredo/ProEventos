26fe8aa25ac5870be1bbe74e2816c2249baa3dce
ericfigueiredo <eric.rq.figueiredo@gmail.com>
2025-06-25 15:33:25 -0300

    Configurando o DbContext no EntityFramework.core

---
+++ DJERICMAX-App.Repositorio/Contexto/DJERICMAX_AppContexto.cs
@@ -0,0 +1,22 @@
+﻿using DJERICMAX_App.Dominio.Entidades;
+using DJERICMAX_App.Dominio.ObjetoDeValor;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DJERICMAX_App.Repositorio.Contexto
+{
+    public class DJERICMAX_AppContexto : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Servico> Servicos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItemPedidos { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+        public DJERICMAX_AppContexto(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- DJERICMAX-App.Web/Startup.cs
+++ DJERICMAX-App.Web/Startup.cs
@@ -1,8 +1,10 @@
+using DJERICMAX_App.Repositorio.Contexto;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,17 +12,24 @@ namespace DJERICMAX_App.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("config.json", optional: false, reloadOnChange: true);
+            Configuration = builder.Build();
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var connectionString = Configuration.GetConnectionString("DJERICMAX_AppDB");
+            services.AddDbContext<DJERICMAX_AppContexto>(option => 
+                                                         option.UseMySql(connectionString, m => 
+                                                                            m.MigrationsAssembly("DJERICMAX-App.Repositorio")));
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
